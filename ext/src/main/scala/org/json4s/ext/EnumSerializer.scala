8f961740f13018492b8aabcbef720154a38f8c11
phani1kumar <phani1kumar@gmail.com>
2015-11-09 10:50:10 +0530

    test again

--- ext/src/main/scala/org/json4s/ext/EnumSerializer.scala
+++ ext/src/main/scala/org/json4s/ext/EnumSerializer.scala
@@ -49,19 +49,25 @@ class EnumNameSerializer[E <: Enumeration: ClassTag](enum: E)
 
   val EnumerationClass = classOf[E#Value]
 
-  def deserialize(implicit format: Formats):
-    PartialFunction[(TypeInfo, JValue), E#Value] = {
-      case (t @ TypeInfo(EnumerationClass, _), json) if (isValid(json)) => {
-        json match {
-         case JString(value) => enum.withName(value)
-          case value => throw new MappingException(s"Can't convert $value to $EnumerationClass")
-        }
+  val longNamePrefix = enum.getClass.getSimpleName.replace("$", "")
+
+  private def stripped(name: String) =
+    if (name.startsWith(longNamePrefix)) name.substring(longNamePrefix.length) else name
+
+  def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), E#Value] = {
+    case (t @ TypeInfo(EnumerationClass, _), json) if isValid(json) => {
+      json match {
+        case JString(value) => enum.withName(stripped(value))
+        case value => throw new MappingException(s"Can't convert $value to $EnumerationClass")
       }
     }
+  }
 
-  private[this] def isValid(json: JValue) = json match {
-    case JString(value) if (enum.values.exists(_.toString == value)) => true
-    case _ => false
+  private[this] def isValid(json: JValue) = {
+    json match {
+      case JString(value) if enum.values.exists(_.toString == stripped(value)) => true
+      case _ => false
+    }
   }
 
   def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
